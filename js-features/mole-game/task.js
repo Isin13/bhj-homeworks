a63f92d9a30ce725223bbf9dd185ce0499f712bb
Isin13 <123600501+Isin13@users.noreply.github.com>
2023-07-05 11:10:32 +0300

    допилил js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,6 +3,7 @@ const holeElements = document.querySelectorAll(".hole");
 let deadCount = 0;
 let lostCount = 0;
 
+
 // Функция для обработки события клика на элемент
 function handleClick(event) {
   const element = event.target;
@@ -11,11 +12,22 @@ function handleClick(event) {
   if (element.classList.contains("hole_has-mole")) {
     // Увеличиваем значение переменной deadCount на 1
     deadCount++;
+    if (deadCount % 10 === 0) {
+      alert('вы победили');
+    
+    }
   } else {
     // Увеличиваем значение переменной lostCount на 1
     lostCount++;
-  }
 
+    // Проверяем, превышает ли значение lostCount 5
+    if (lostCount > 5) {
+      lostCount = 0;
+      deadCount = 0;
+      confirm("проиграл. ещё игру?");
+  
+    }
+  }
   // Обновляем значения переменных в HTML
   document.getElementById("dead").textContent = deadCount;
   document.getElementById("lost").textContent = lostCount;
@@ -25,5 +37,3 @@ function handleClick(event) {
 holeElements.forEach((element) => {
   element.addEventListener("click", handleClick);
 });
-
-
