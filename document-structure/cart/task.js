dbeec0da2278e01d8042c69aac4777d1a3efbd66
Isin13 <123600501+Isin13@users.noreply.github.com>
2023-08-25 21:02:24 +0300

    help me

---
+++ document-structure/cart/task.js
@@ -0,0 +1,16 @@
+const basket = document.querySelector('cart__products'); //корзина
+//первый продукт
+const reduceProduct1 = document.querySelector('product__quantity-control_dec');// - 
+const productIncrease1 = document.querySelector('product__quantity-control_inc');// + 
+const quantityValue1 = document.querySelector('product__quantity-value');// количество
+const productAdd1 = document.querySelector('cart__products');// добавить в корзину
+//второй продукт
+const reduceProduct2 = document.querySelector('product__quantity-control_dec');// - 
+const productIncrease2 = document.querySelector('product__quantity-control_inc');// + 
+const quantityValue2 = document.querySelector('product__quantity-value');// количество
+const productAdd2 = document.querySelector('cart__products');// добавить в корзину
+//третий продукт
+const reduceProduct3 = document.querySelector('product__quantity-control_dec');// - 
+const productIncrease3 = document.querySelector('product__quantity-control_inc');// + 
+const quantityValue3 = document.querySelector('product__quantity-value');// количество
+const productAdd3 = document.querySelector('cart__products');// добавить в корзину
