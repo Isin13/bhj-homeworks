fd4b023187087215e574d99d6bfb80a0eb54b69e
Isin13 <123600501+Isin13@users.noreply.github.com>
2023-08-29 19:26:05 +0300

    commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,16 +1,64 @@
-const basket = document.querySelector('cart__products'); //корзина
-//первый продукт
-const reduceProduct1 = document.querySelector('product__quantity-control_dec');// - 
-const productIncrease1 = document.querySelector('product__quantity-control_inc');// + 
-const quantityValue1 = document.querySelector('product__quantity-value');// количество
-const productAdd1 = document.querySelector('cart__products');// добавить в корзину
-//второй продукт
-const reduceProduct2 = document.querySelector('product__quantity-control_dec');// - 
-const productIncrease2 = document.querySelector('product__quantity-control_inc');// + 
-const quantityValue2 = document.querySelector('product__quantity-value');// количество
-const productAdd2 = document.querySelector('cart__products');// добавить в корзину
-//третий продукт
-const reduceProduct3 = document.querySelector('product__quantity-control_dec');// - 
-const productIncrease3 = document.querySelector('product__quantity-control_inc');// + 
-const quantityValue3 = document.querySelector('product__quantity-value');// количество
-const productAdd3 = document.querySelector('cart__products');// добавить в корзину
+// Назначаем обработчик события на все кнопки "Добавить в корзину"
+let addToCartButtons = document.querySelectorAll('.product__add');
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
+
+// Назначаем обработчики событий на все кнопки увеличения и уменьшения количества товара
+let incrementButtons = document.querySelectorAll('.product__quantity-control_inc');
+let decrementButtons = document.querySelectorAll('.product__quantity-control_dec');
+incrementButtons.forEach(button => {
+  button.addEventListener('click', incrementQuantity);
+});
+decrementButtons.forEach(button => {
+  button.addEventListener('click', decrementQuantity);
+});
+
+function addToCart(event) {
+  let product = event.target.closest('.product');
+  let productId = product.dataset.id;
+  let quantity = parseInt(product.querySelector('.product__quantity-value').textContent);if (quantity <= 0) return;
+  
+  let cartProduct = document.querySelector(`.cart__product[data-id="${productId}"]`);
+  
+  if (cartProduct) {
+    let cartProductQuantity = parseInt(cartProduct.querySelector('.cart__product-count').textContent);
+    cartProduct.querySelector('.cart__product-count').textContent = cartProductQuantity + quantity;
+  } else {
+    let cartProducts = document.querySelector('.cart__products');
+    
+    let cartProduct = document.createElement('div');
+    cartProduct.classList.add('cart__product');
+    cartProduct.dataset.id = productId;
+    
+    let productImage = product.querySelector('.product__image');
+    let cartProductImage = document.createElement('img');
+    cartProductImage.classList.add('cart__product-image');
+    cartProductImage.src = productImage.src;
+    
+    let cartProductCount = document.createElement('div');
+    cartProductCount.classList.add('cart__product-count');
+    cartProductCount.textContent = quantity;
+    
+    cartProduct.appendChild(cartProductImage);
+    cartProduct.appendChild(cartProductCount);
+    
+    cartProducts.appendChild(cartProduct);
+  }
+  
+  product.querySelector('.product__quantity-value').textContent = '1';
+}
+
+function incrementQuantity(event) {
+  let quantityValue = event.target.parentNode.querySelector('.product__quantity-value');
+  let currentQuantity = parseInt(quantityValue.textContent);
+  quantityValue.textContent = currentQuantity + 1;
+}
+
+function decrementQuantity(event) {
+  let quantityValue = event.target.parentNode.querySelector('.product__quantity-value');
+  let currentQuantity = parseInt(quantityValue.textContent);
+  if (currentQuantity > 1) {
+    quantityValue.textContent = currentQuantity - 1;
+  }
+}
